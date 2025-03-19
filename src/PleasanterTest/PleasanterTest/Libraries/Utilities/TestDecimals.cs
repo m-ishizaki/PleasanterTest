ef523f77c332efb7e4c7bcc5c8cd61988a1286b5
m-ishizaki <love.ms.is12t@hotmail.co.jp>
2025-03-20 02:28:36 +0900

    add  Test Decimals

---
+++ src/PleasanterTest/PleasanterTest/Libraries/Utilities/TestDecimals.cs
@@ -0,0 +1,20 @@
+﻿namespace PleasanterTest.Libraries.Utilities
+{
+    [TestClass]
+    public class TestDecimals
+    {
+        private static void M(decimal input) => Assert.AreEqual(input.ToString("G29"), Implem.Libraries.Utilities.Decimals.TrimEndZero(input));
+        private static void M(decimal? input) => Assert.AreEqual(input?.ToString("G29") ?? "0", Implem.Libraries.Utilities.Decimals.TrimEndZero(input));
+
+        [TestMethod]
+        public void TrimEndZero_ShouldReturnZero_WhenInputIsZero() => M(0m);
+        [TestMethod]
+        public void TrimEndZero_ShouldReturnSameNumber_WhenInputHasNoTrailingZeros() => M(123.45m);
+        [TestMethod]
+        public void TrimEndZero_ShouldReturnTrimmedNumber_WhenInputHasTrailingZeros() => M(123.4500m);
+        [TestMethod]
+        public void TrimEndZero_ShouldReturnTrimmedNumber_WhenInputHasTrailingZeros29() => M(9.999_000_000_000_000_000_000_000_000m);
+        [TestMethod]
+        public void TrimEndZero_ShouldReturnTrimmedNumber_WhenInputIsNull() => M(null);
+    }
+}
