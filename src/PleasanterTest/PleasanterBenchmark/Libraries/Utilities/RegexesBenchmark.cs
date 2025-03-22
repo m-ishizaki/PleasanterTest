7aebf6e01102096cd34dce5ac4670787db90a054
m-ishizaki <love.ms.is12t@hotmail.co.jp>
2025-03-22 17:31:15 +0900

    wip

---
+++ src/PleasanterTest/PleasanterBenchmark/Libraries/Utilities/RegexesBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using Implem.Libraries.Utilities;
+using RkSoftware.TinyBenchmark.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PleasanterBenchmark.Libraries.Utilities
+{
+    public class RegexesBenchmark
+    {
+        [TinyBenchmark] public static void TestRegexFirst_MatchFound() => "Hello World".RegexFirst("World");
+        [TinyBenchmark] public static void TestRegexFirst_NoMatch() => "Hello World".RegexFirst("Universe");
+        [TinyBenchmark] public static void TestRegexExists_MatchFound() => "Hello World".RegexExists("World");
+        [TinyBenchmark] public static void TestRegexExists_NoMatch() => "Hello World".RegexExists("Universe");
+        [TinyBenchmark] public static void TestRegexMatches_MatchFound() => "Hello World".RegexMatches("World");
+        [TinyBenchmark] public static void TestRegexLike_MatchFound() => "Hello World".RegexLike("world");
+        [TinyBenchmark] public static void TestRegexMatches_NoMatch() => "Hello World".RegexMatches("Universe");
+        [TinyBenchmark] public static void TestRegexLike_NoMatch() => "Hello World".RegexLike("universe");
+        [TinyBenchmark] public static void TestRegexValues_MatchFound() => "Hello World".RegexValues("World").ToList();
+        [TinyBenchmark] public static void TestRegexValues_NoMatch() => "Hello World".RegexValues("Universe").ToList();
+    }
+}
--- src/PleasanterTest/PleasanterBenchmark/Program.cs
+++ src/PleasanterTest/PleasanterBenchmark/Program.cs
@@ -6,9 +6,11 @@ using System.Diagnostics;
 
 var sw = Stopwatch.StartNew();
 //var _ = BenchmarkRunner.Run<PleasanterBenchmark.Libraries.Utilities.BenchmarkDecimals>();
-var results = TinyBenchmarkRunner.Run<PleasanterBenchmark.Libraries.Utilities.BenchmarkDecimals>();
+var results1 = TinyBenchmarkRunner.Run<PleasanterBenchmark.Libraries.Utilities.BenchmarkDecimals>();
+var results2 = TinyBenchmarkRunner.Run<PleasanterBenchmark.Libraries.Utilities.RegexesBenchmark>();
 sw.Stop();
 Console.WriteLine($"TotalSeconds: {sw.Elapsed.TotalSeconds} -");
 Console.WriteLine();
 
-TinyBenchmarkFormatter.Write(results, System.Console.WriteLine);
+TinyBenchmarkFormatter.Write(results1, System.Console.WriteLine);
+TinyBenchmarkFormatter.Write(results2, System.Console.WriteLine);
